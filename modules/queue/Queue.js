9d882ef8a3ef73cc798815ddb749dcdc5848cf8b
Carlo Paredes <c.nick.paredes@gmail.com>
2020-08-03 16:01:43 -0700

    Added queue implementation

--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,23 +1,62 @@
+class _Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
 class Queue {
   constructor() {
-    // Set initial data.
+    this.first = null,
+    this.last = null
   }
 
   enqueue(data) {
-    // Add some data to the queue.
+    const node = new _Node(data);
+
+    if (this.first === null) {
+      this.first = node;
+    }
+
+    if (this.last) {
+      this.last.next = node;
+    }
+
+    this.last = node;
   }
 
   dequeue() {
-    // Remove some data from the queue.
+    if (this.first === null) {
+      return;
+    }
+
+    const node = this.first;
+    this.first = this.first.next;
+
+    if (node === this.last) {
+      this.last = null;
+    }
+    return node.data;
   }
 
   show() {
     // Return the next item in the queue.
+    return this.first.data;
   }
 
   all() {
     // Return all items in the queue.
+    let currNode = this.first;
+    let store = [];
+    
+    while(currNode !== null) {
+      store.push(currNode.data);
+      currNode = currNode.next;
+    }
+
+    return store;
   }
 }
 
 module.exports = Queue
+
