fe629316e7baf1cdb8b8bd7b165234e16de4a6cb
Thomas Davis <ThomasDavisA@gmail.com>
2020-08-10 20:53:09 -0700

    cors stuff

--- modules/app/app.js
+++ modules/app/app.js
@@ -5,9 +5,7 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors({
-    origin: CLIENT_ORIGIN
-}))
+app.use(cors)
 
 app.use('/people', require('../people/people.router'))
 app.use('/pets', require('../pets/pets.router'))
